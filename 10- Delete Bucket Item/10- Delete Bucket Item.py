4a52f76bc5a89d5d678cc09a705a1adbccc949c4
Eng.\ Tharwat El-Sayed <tharwat_uss89@hotmail.com>
2025-10-29 07:35:33 +0300

    Remove Supabase credentials from delete script
    Removed sensitive Supabase credentials from the script.

--- 10- Delete Bucket Item/10- Delete Bucket Item.py
+++ 10- Delete Bucket Item/10- Delete Bucket Item.py
@@ -1,8 +1,8 @@
 from supabase import create_client, Client
 
 # --- Supabase project credentials ---
-url = "https://lnkqupiklbgvdmfnyjlj.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxua3F1cGlrbGJndmRtZm55amxqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTMyODc3OCwiZXhwIjoyMDc2OTA0Nzc4fQ.QSzKG0kQywl4wnub1xu8-xpNpMm8kVlwhL8gqagk6p4"  # Use your Service Role Key for delete permissions
+url = ""
+key = ""  # Use your Service Role Key for delete permissions
 
 try:
     # Initialize Supabase client
@@ -25,3 +25,4 @@ try:
 
 except Exception as e:
     print("❌ Error while deleting file:", e)
+
